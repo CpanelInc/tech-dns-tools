b3e98e7d3de61f53e1be25c2d9a4240c1e479798
Brian Warren <brian.scott.warren@gmail.com>
2013-05-20 17:43:00 -0500

    Initial commit

---
+++ nscheck.pl
@@ -0,0 +1,266 @@
+#!/usr/bin/perl
+use 5.008008;
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+my $domain = shift(@ARGV);
+$domain =~ s/[\.]+$//; # Remove any trailing dots
+my @domain_parts = split(/\./, $domain);
+unless ($domain && @domain_parts.length > 1) {
+    usage('A valid domain name is required.')
+}
+
+my $hr = "\n" . '-' x 50 . "\n";
+
+# Enable the ability to conditionally use some modules
+# Domain::PublicSuffix - derive root domain and effective TLD.
+# Net::DNS - perform DNS queries
+# LWP::Simple - perform simple web requests
+my @modules = ('Domain::PublicSuffix', 'Net::DNS', 'LWP::Simple',
+    'Mail::ClamAV');
+my %module_available;
+foreach (@modules) {
+    $module_available{$_} = import_module_if_found($_);
+}
+
+# Globals
+my $root_domain;
+my @suffixes_to_query;
+my $effective_tld_names_file = '/tmp/effective_tld_names.dat';
+
+# Download effective_tld_names.dat
+if ($module_available{'LWP::Simple'}) {
+    if (-M $effective_tld_names_file > 7) { # Download if a week old or more
+        my $url = 'http://mxr.mozilla.org/mozilla-central/source/netwerk' .
+            '/dns/effective_tld_names.dat?raw=1';
+        getstore($url, $effective_tld_names_file);
+    }
+}
+
+# Use Domain::PublicSuffix if available to determine public suffix, also called
+# "effective TLD".
+if ($module_available{'Domain::PublicSuffix'}) {
+    my $public_suffix = Domain::PublicSuffix->new({
+        'data_file' => $effective_tld_names_file
+    });
+    $root_domain = $public_suffix->get_root_domain($domain);
+
+    if ( $public_suffix->error ) {
+        printf( "%12s: %s\n", 'Error', $public_suffix->error );
+        exit(1);
+    }
+    else {
+        push(@suffixes_to_query, $public_suffix->suffix);
+        if ($public_suffix->tld ne $public_suffix->suffix) {
+            push(@suffixes_to_query, $public_suffix->tld);
+        }
+    }    
+}
+# If Domain::PublicSuffix is not available, figure out suffixes using
+# downloaded database file
+else {
+    my $tld = $domain_parts[-1];
+    my $etld = join('.', @domain_parts[-2..-1]);
+    if (-e $effective_tld_names_file) {
+        open SUFFIX_FILE, '<', $effective_tld_names_file;
+        while (<SUFFIX_FILE>) {
+            if (/^\/\/ $tld/../^$/) {
+                next if /^\/\// || /^$/;
+                #$_ =~ s/^\s+$//;
+                chomp ( my $this_suffix = $_ );
+                if ($tld =~ /${this_suffix}$/ || $etld =~ /${this_suffix}$/) {
+                    unshift(@suffixes_to_query, $this_suffix);
+                }
+            }
+        }
+        # Trim off any subdomain components in case they were included by user
+        $root_domain = $domain;
+        foreach(@suffixes_to_query) {
+            if ($_ =~ /\./) {
+                $root_domain =~ s/\.$_$//;
+                my @stem_parts = split(/\./, $root_domain);
+                $root_domain = $stem_parts[-1] . '.' . $_;
+                last;
+            }
+        }
+        unless ($root_domain) {
+            $root_domain = join(splice(@domain_parts, -2, 2), '.');
+        }
+    }
+    # If no database file exists, assume that user provided root domain and
+    # that the last part is the TLD. If the domain has three parts, then
+    # assume that the last two parts is the public suffix
+    else {
+        # Prune domain so it has just 3 parts (assumes that the suffix is no
+        # more that two parts)
+        my @root_domain_parts = @domain_parts;
+        if (@domain_parts.length > 3) {
+            @root_domain_parts = splice(@domain_parts, -3, 3);
+        }
+        $root_domain = join(@root_domain_parts, '.');
+        if (@root_domain_parts.length > 2) {
+            my $etld = join(splice(@root_domain_parts, -2,2), '.');
+            push(@suffixes_to_query, $etld); # Add public suffix
+        }
+        push(@suffixes_to_query, $domain_parts[-1]); # Add TLD
+    }
+}
+
+print $hr;
+printf( "%12s: %s\n", 'Domain', $domain );
+printf("%12s: %s\n", 'Root Domain', $root_domain );
+printf("%12s: %s\n", 'Suffix', $suffixes_to_query[0]);
+printf("%12s: %s", 'TLD', $suffixes_to_query[1]);
+print $hr;
+
+# Get nameservers for the effective tld and the true tld
+#my $previous_suffix = '';
+for (my $i = 0; $i < @suffixes_to_query.length; $i++) {
+    my $suffix = $suffixes_to_query[$i];
+    printf("%s%s %s%s", $hr, "Suffix:", $suffix, $hr);
+    next unless (my @names = get_nameservers($suffix));
+    printf("%s\n", 'TLD Servers:');
+    my @ips;
+    for (my $j = 0; $j < @names.length; $j++) {
+        my $name = $names[$j];
+        my $ip = a_lookup($name);
+        push(@ips, $ip);
+        print "$name $ip\n";
+    }
+    # Ask the TLD servers for authoritative nameservers of domain
+    print suffix_nameserver_report($root_domain, \@ips);
+}
+
+sub get_nameservers {
+    my $this_domain = shift;
+    if ($module_available{'Net::DNS'}) {
+        return get_nameservers_Net_Dns($this_domain);
+    }
+    return get_nameservers_dig($this_domain);
+}
+
+sub get_nameservers_Net_Dns {
+    my $this_domain = shift;
+    my $res = Net::DNS::Resolver->new;
+    my $query = $res->query("${this_domain}.", 'NS');
+    my @nameservers;
+    if ($query) {
+        foreach my $rr (grep { $_->type eq 'NS' } $query->answer) {
+            push(@nameservers, $rr->nsdname);
+        }
+    }
+    else {
+        warn "query failed: ", $res->errorstring, "\n";
+    }
+    return @nameservers;
+}
+
+sub get_nameservers_dig {
+    my $this_domain = shift;
+    chomp( my $result = qx(dig NS \@8.8.8.8 ${this_domain}. +short) );
+    $result =~ s/\.$//;
+    my @result = split(/\.\n/, $result);
+}
+
+sub a_lookup {
+    my $this_domain = shift;
+    if ($module_available{'Net::DNS'}) {
+        return a_lookup_Net_Dns($this_domain);
+    }
+    return a_lookup_dig($this_domain);
+}
+
+sub a_lookup_Net_Dns {
+    my $this_domain = shift;
+    my $res = Net::DNS::Resolver->new;
+    my $query = $res->query("${this_domain}.", 'A');
+    my @answers;
+    if ($query) {
+        foreach my $rr (grep { $_->type eq 'A' } $query->answer) {
+            push(@answers, $rr->address);
+        }
+    }
+    else {
+        warn "query failed: ", $res->errorstring, "\n";
+    }
+    return $answers[0] || '';
+}
+
+sub a_lookup_dig {
+    my $this_domain = shift;
+    chomp( my $result = qx(dig A \@8.8.8.8 ${this_domain}. +short) );
+    my @answers = split(/\n/, $result);
+    return $answers[0] || '';
+}
+
+sub suffix_nameserver_report {
+    my $this_domain = $_[0];
+    my @suffix_nameserver_ips = @{$_[1]};
+    if ($module_available{'Net::DNS'}) {
+        return suffix_nameserver_report_Net_Dns($this_domain,
+            \@suffix_nameserver_ips);
+    }
+    return suffix_nameserver_report_dig($this_domain, \@suffix_nameserver_ips);
+}
+
+sub suffix_nameserver_report_Net_Dns {
+    my $this_domain = $_[0];
+    my @suffix_nameserver_ips = @{$_[1]};
+    my $res = Net::DNS::Resolver->new(
+        nameservers => [(@suffix_nameserver_ips)],
+        recurse => 0,
+      	debug => 0,
+    );
+    # TODO: Should this be an A query, or NS, or what?
+    my $packet = $res->send("${this_domain}.", 'A');
+    my @authority = $packet->authority;
+    my @additional = $packet->additional;
+    my $result = "\nAUTHORITY:\n";
+    foreach my $rr (@authority) {
+        $result .= $rr->string . "\n";
+    }
+    $result .= "\nADDITIONAL:\n";
+    foreach my $rr (@additional) {
+        $result .= $rr->string . "\n";
+    }
+    return $result . "\n";
+}
+
+sub suffix_nameserver_report_dig {
+    my $this_domain = $_[0];
+    my @suffix_nameserver_ips = @{$_[1]};
+    # TODO: find out why A query causes AUTHORITY section to show up but when
+    # querying for NS record, only ADDITIONAL shows up (not sure which is
+    # right to begin with as we are not really looking for an answer, but
+    # gleaning the nameserver names and glue records from the TLD servers)
+    my $cmd = "dig \@$suffix_nameserver_ips[0] A $this_domain." .
+        ' +noall +authority +additional +comments';
+    print "$cmd\n";
+    chomp( my $result = qx($cmd) );
+    return $result;
+}
+
+sub usage {
+	my ( $error ) = @_;
+	print "get_root_domain - Domain::PublicSuffix " .
+        $Domain::PublicSuffix::VERSION . "\n";
+	print "Usage: get_root_domain <domainname>\n";
+	exit(1);
+}
+
+sub import_module_if_found {
+    my $module = shift;
+    eval {
+        (my $file = $module) =~ s|::|/|g;
+        require $file . '.pm';
+        $module->import();
+        return 1;
+        #return 0; # For testing case where modules not available
+    } or do {
+        return 0;
+    }
+}
+
+1;
